5f9af380ea6ab2074a1abc2f5b19ff3d4f87eae1
tolbarroso <carol.00000829955@unicap.br>
2022-09-27 18:33:03 -0300

    commit

--- LSE/TesteListaEncadeada.java
+++ LSE/TesteListaEncadeada.java
@@ -4,7 +4,9 @@ public class TesteListaEncadeada {
 
         public static void main(String[] args) {
                 Scanner in = new Scanner(System.in);
-                LSEAluno turma = new LSEAluno();
+                LSEAluno turmaM = new LSEAluno();
+                LSEAluno turmaP = new LSEAluno();
+                LSEAluno turmaUniao = new LSEAluno();
                 int op;
                 Aluno al;
                 String nome, matr;
@@ -22,7 +24,8 @@ public class TesteListaEncadeada {
                                         System.out.print("Informe a matrícula do aluno: ");
                                         matr = in.nextLine();
                                         al = new Aluno(matr, nome);
-                                        turma.inserirAluno(al);
+                                        turmaM.inserirAluno(al);
+                                        turmaUniao.inserirAlunoPorUltimo(al);
                                         break;
                                 case 2: // cadastrar aluno no final
                                         System.out.print("Informe o nome do aluno: ");
@@ -30,10 +33,11 @@ public class TesteListaEncadeada {
                                         System.out.print("Informe a matrícula do aluno: ");
                                         matr = in.nextLine();
                                         al = new Aluno(matr, nome);
-                                        turma.inserirAlunoPorUltimo(al);
+                                        turmaM.inserirAlunoPorUltimo(al);
+                                        turmaUniao.inserirAlunoPorUltimo(al);
                                         break;
                                 case 3:
-                                        turma.exibir();
+                                        turmaM.exibir();
                                         break;
                                 case 4: // método de busca para pegar os dados de um único aluno
                                         System.out.println("Insira o nome do aluno: ");
@@ -41,7 +45,7 @@ public class TesteListaEncadeada {
                                         System.out.println("Informe a matrícula do aluno: ");
                                         matr = in.nextLine();
                                         al = new Aluno(matr, nome);
-                                        turma.exibirEspecifico(al);
+                                        turmaM.exibirEspecifico(al);
                                         break;
                                 case 5: // get e set nova média de um aluno
                                         System.out.println("Insira o nome do aluno: ");
@@ -51,7 +55,7 @@ public class TesteListaEncadeada {
                                         // al = new Aluno(matr, nome);
                                         System.out.println("Insira a nova média do aluno: ");
                                         media = in.nextDouble();
-                                        turma.inserirNovaMedia(nome, matr, media);
+                                        turmaM.inserirNovaMedia(nome, matr, media);
                                         break;
                                 case 6: // get e set nova falta de um aluno
                                         System.out.println("Insira o nome do aluno: ");
@@ -61,12 +65,67 @@ public class TesteListaEncadeada {
                                         al = new Aluno(matr, nome);
                                         System.out.println("Insira a nova média do aluno: ");
                                         faltas = in.nextInt();
-                                        turma.inserirNovasFaltas(al, faltas);
+                                        turmaM.inserirNovasFaltas(al, faltas);
                                         break;
                                 case 7: // método de busca para achar um aluno e remover-lo
                                         System.out.println("Insira a matrícula do aluno a ser removido: ");
                                         matr = in.nextLine();
-                                        turma.removerAlunoEspecifico(matr);
+                                        turmaM.removerAlunoEspecifico(matr);
+                                        break;
+                                case 8: // cadastrar aluno no início
+                                        System.out.print("Informe o nome do aluno: ");
+                                        nome = in.nextLine();
+                                        System.out.print("Informe a matrícula do aluno: ");
+                                        matr = in.nextLine();
+                                        al = new Aluno(matr, nome);
+                                        turmaP.inserirAluno(al);
+                                        break;
+                                case 9: // cadastrar aluno no final
+                                        System.out.print("Informe o nome do aluno: ");
+                                        nome = in.nextLine();
+                                        System.out.print("Informe a matrícula do aluno: ");
+                                        matr = in.nextLine();
+                                        al = new Aluno(matr, nome);
+                                        turmaP.inserirAlunoPorUltimo(al);
+                                        break;
+                                case 10:
+                                        turmaP.exibir();
+                                        break;
+                                case 11: // método de busca para pegar os dados de um único aluno
+                                        System.out.println("Insira o nome do aluno: ");
+                                        nome = in.nextLine();
+                                        System.out.println("Informe a matrícula do aluno: ");
+                                        matr = in.nextLine();
+                                        al = new Aluno(matr, nome);
+                                        turmaP.exibirEspecifico(al);
+                                        break;
+                                case 12: // get e set nova média de um aluno
+                                        System.out.println("Insira o nome do aluno: ");
+                                        nome = in.nextLine();
+                                        System.out.println("Informe a matrícula do aluno: ");
+                                        matr = in.nextLine();
+                                        // al = new Aluno(matr, nome);
+                                        System.out.println("Insira a nova média do aluno: ");
+                                        media = in.nextDouble();
+                                        turmaP.inserirNovaMedia(nome, matr, media);
+                                        break;
+                                case 13: // get e set nova falta de um aluno
+                                        System.out.println("Insira o nome do aluno: ");
+                                        nome = in.nextLine();
+                                        System.out.println("Informe a matrícula do aluno: ");
+                                        matr = in.nextLine();
+                                        al = new Aluno(matr, nome);
+                                        System.out.println("Insira a nova média do aluno: ");
+                                        faltas = in.nextInt();
+                                        turmaP.inserirNovasFaltas(al, faltas);
+                                        break;
+                                case 14: // método de busca para achar um aluno e remover-lo
+                                        System.out.println("Insira a matrícula do aluno a ser removido: ");
+                                        matr = in.nextLine();
+                                        turmaP.removerAlunoEspecifico(matr);
+                                        break;
+                                case 15: // método para unir duas listas
+                                        turmaUniao.exibir();
                                         break;
                                 case 0:
                                         System.out.println("Programa encerrado!");
@@ -79,13 +138,21 @@ public class TesteListaEncadeada {
 
         public static void exibeMenu() {
                 System.out.println("Opções:");
-                System.out.println("1 - Inserir um novo aluno no início da turma");
-                System.out.println("2 - Inserir um novo aluno no final da turma");
-                System.out.println("3 - Exibir todos os alunos da turma");
-                System.out.println("4 - Exibir dados de um único aluno da turma");
-                System.out.println("5 - Alterar média de um aluno");
-                System.out.println("6 - Alterar a quantidade de faltas de um aluno");
-                System.out.println("7 - Remover um aluno da turma");
+                System.out.println("1 - Inserir um novo aluno no início da turma de Matemática");
+                System.out.println("2 - Inserir um novo aluno no final da turma de Matemática");
+                System.out.println("3 - Exibir todos os alunos da turma de Matemática");
+                System.out.println("4 - Exibir dados de um único aluno da turma de Matemática");
+                System.out.println("5 - Alterar média de um aluno de Matemática");
+                System.out.println("6 - Alterar a quantidade de faltas de um aluno de Matemática");
+                System.out.println("7 - Remover um aluno da turma de Matemática");
+                System.out.println("8 - Inserir um novo aluno no início da turma de Português");
+                System.out.println("9 - Inserir um novo aluno no final da turma de Português");
+                System.out.println("10 - Exibir todos os alunos da turma de Português");
+                System.out.println("11 - Exibir dados de um único aluno da turma de Português");
+                System.out.println("12 - Alterar média de um aluno de Português");
+                System.out.println("13 - Alterar a quantidade de faltas de um aluno de Português");
+                System.out.println("14 - Remover um aluno da turma de Português");
+                System.out.println("15 - Exibir lista geral das turmas");
                 System.out.println("0 - Sair do programa");
                 System.out.print("Digite a opção desejada: ");
         }
